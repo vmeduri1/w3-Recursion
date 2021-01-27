579abbdb7827b3d8b602b15c396e98486e173be2
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-01-26 18:24:26 -0600

    Final Commiot

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -11,7 +11,30 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 // your code here
-  
+
+function flatten (array) {
+  debugger
+  let newArray = [];
+
+  array.forEach((element, i) => {
+    if (Array.isArray(element)) {
+      newArray.push(...flatten(element));
+    } else {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+}
+
+// console.log(flatten([])); // []
+// console.log(flatten([1, 2])); // [1, 2]
+// console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
+console.log(flatten([
+  [1, 2],
+  [3, [4, 5]],
+]));
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = flatten;
